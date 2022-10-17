6eb207fceb67cb11c1510269cd14bca334f88072
gnuion <gnu.ion@gmail.com>
2022-10-17 20:19:11 +0200

    Modify build.sh for building stor

--- builder/build.sh
+++ builder/build.sh
@@ -2,11 +2,11 @@ set -e
 
 # docker build --tag gnuion/chia-builder:py3.10 .
 
-if [ -d "stai-blockchain" ]; then rm -Rf stai-blockchain; fi
+if [ -d "stor-blockchain" ]; then rm -Rf stor-blockchain; fi
 
-mkdir stai-blockchain && cd stai-blockchain
+mkdir stor-blockchain && cd stor-blockchain
 
-git clone https://github.com/STATION-I/stai-blockchain.git
+git clone https://github.com/Stor-Network/stor-blockchain.git .
 
-docker run -v $PWD:/opt/stai-blockchain gnuion/chia-builder:py3.10 ash -c "\
-cd /opt/stai-blockchain && sh install.sh"
+docker run -v $PWD:/opt/stor-blockchain gnuion/chia-builder:py3.10 ash -c "\
+cd /opt/stor-blockchain && sh install.sh"
